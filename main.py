c4d72486bc548637b6fd762b89f08063b4761010
Lukáš Bystroň <lbystron@gmail.com>
2025-05-22 22:53:31 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 import argparse
 
 parser = argparse.ArgumentParser(
-    prog="Web scraping. ČR volby 2017"
+    prog="Projekt: Elections Scraper"
 )
 parser.add_argument(
     "url", type=str, help="Stahuji data z vybraného url:"
@@ -21,10 +21,7 @@ parser.add_argument(
 )
 args = parser.parse_args()
 url = args.url
-if args.url == 2: #očekává dva argumenty
-    print("Špatný argument")
-else:
-    print(f"Stahuji data z vybraneho url: {url}")
+print(f"Stahuji data z vybraneho url: {url}")
 
 def download_www():
     """
@@ -57,6 +54,7 @@ def stranky_webu(cislo) -> list:
     vsechny_radky = []
 
     for obec in cislo:
+        #Dosadí číslo obce a načte novou stranku pro stahování dat.
         stranka_webu = (f"https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=14&xobec="
               f"{int(obec["Kód obce"])}&xvyber=8105")
         odpoved = requests.get(stranka_webu)
@@ -74,9 +72,12 @@ def kod_nazev_obce(table) -> list:
 
     vsechny_tr = table.find_all("tr")
     for radek in vsechny_tr:
+        #Najdi číslo obce
         kod_obce = radek.find("td", {"class": "cislo"})
+        #Najdi název obce
         nazev_obce = radek.find("td", {"class": "overflow_name"})
 
+        # Odstraní všechný jiné znaky. Výstup text
         if kod_obce and nazev_obce:
             kod_nazev_obce.append({
                 "Kód obce": kod_obce.text.strip(),
@@ -92,10 +93,14 @@ def volici_obalky_hlasy(stranka: list) -> list:
     seznam = []
 
     for radek in stranka:
+        #Najdi počet voličů v seznamu
         volici_seznam = radek.find("td", {"headers": "sa2"})
+        #Najdi počet vydaných obálek
         vydane_obalky = radek.find("td", {"headers": "sa3"})
+        #Najdi počet platných hlasů
         platne_hlasy = radek.find("td", {"headers": "sa6"})
 
+        # Odstraní všechný jiné znaky. Výstup text
         if volici_seznam and vydane_obalky and platne_hlasy:
             seznam.append({
                 "Voliči v seznamu": volici_seznam.text.strip(),
@@ -113,14 +118,19 @@ def nazev_hlasy_volebni_strany(stranka: list) -> list:
     nazev = []
 
     for radek in stranka:
+        #Najdi název volební strany
         nazev_strany = radek.find("td", {"class": "overflow_name"})
+        #Najdi v první tabulce hlasy
         hlasy_tabulka_1 = radek.find("td", {"headers": "t1sa2 t1sb3"})
+        #Najdi v druhé tabulce hlasy
         hlasy_tabulka_2 = radek.find("td", {"headers": "t2sa2 t2sb3"})
 
+        #Odstraní všechný jiné znaky. Výstup text
         if hlasy_tabulka_1:
             nazev.append({
                 nazev_strany.text.strip(): hlasy_tabulka_1.text.strip(),
             })
+        #Odstraní všechný jiné znaky. Výstup text
         if hlasy_tabulka_2:
             nazev.append({
                 nazev_strany.text.strip(): hlasy_tabulka_2.text.strip(),
@@ -154,13 +164,13 @@ def vytvor_csv():
     vyber_obce = volici_obalky_hlasy(otaceni_stranek)
     volebni_strana = nazev_hlasy_volebni_strany(otaceni_stranek)
     data = urovnani_dat(volebni_strana)
-    keys_list = list(data.keys())
+    strana_nazev = list(data.keys())
 
-    print("Ukladam do souboru: vysledky_opava.csv")
+    print("Ukládám do souboru: vysledky_opava.csv")
     with open("vysledky_opava.csv", "w", newline="", encoding="utf-8") as file:
         #názvy sloupců tabulky
         fieldnames = ["Kód obce", "Název obce", "Voliči v seznamu",
-                      "Vydané obálky", "Platné hlasy"] + keys_list # Název volební strany
+                      "Vydané obálky", "Platné hlasy"] + strana_nazev # Název volební strany
         writer = csv.DictWriter(file, fieldnames=fieldnames)
 
         writer.writeheader()  # Hlavička tabulky
@@ -175,7 +185,7 @@ def vytvor_csv():
             for nazev_strany, pocet_hlasu in zip(data.keys(), hlasy):
                 row[nazev_strany] = pocet_hlasu
             writer.writerow(row)
-        print("Ukončuji web scraping")
+        print("Ukončuji election-scraper")
 
 if __name__ == '__main__':
     #spuštění programu
--- readme.md
+++ readme.md
@@ -1,27 +1,16 @@
-# Projekt 3 - Web scraping volebních výsledků
+# ENGETO Projekt 3 - Web scraping volebních výsledků
 
-Tento projekt je součástí Engeto Online Python Akademie. Cílem je stáhnout a zpracovat volební výsledky z webové stránky https://www.volby.cz/ a uložit je do CSV souboru.
+Tento projekt je součástí Engeto Online Python Akademie. Cílem je stáhnout a zpracovat volební výsledky z webové stránky [https://www.volby.cz/pls/ps2017nss/ps3?xjazyk=CZ](https://www.volby.cz/pls/ps2017nss/ps3?xjazyk=CZ) a uložit je do CSV souboru.
 
 Volby do Poslanecké sněmovny Parlamentu České republiky konané ve dnech 20.10. – 21.10.2017
 
-![Image](https://github.com/user-attachments/assets/0619e05f-4d89-4d15-b7e2-d21969f3b67b)
+![Image](https://github.com/user-attachments/assets/f9875e6e-3d55-49f8-a1c8-c10ddd0259c8)
 
 ## Ukázka projektu
   1. argument: `"https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=14&xnumnuts=8105"`
   2. argument: `"vysledky_opava.csv"`
 
-## Struktura projektu
-- `main.py` - Hlavní skript pro stažení a zpracování dat.
-- `vysledky_opava.csv` - Výstupní soubor obsahující zpracovaná data.
-- `requirements.txt` - Seznam závislostí potřebných pro spuštění projektu.\
-
-## Ukázka kódu
-
-`'Občanská demokratická strana': ['22', '48', '99', '37', '2', '46', '31', '164', '46', '5', '48', '37', '14']`
-`'Řád národa - Vlastenecká unie': ['0', '3', '1', '0', '0', '0', '0', '1', '0', '0', '0', '0', '1', '0', '0']`
-`'CESTA ODPOVĚDNÉ SPOLEČNOSTI': ['0', '1', '3', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0']`
-
-## Použité technologie
+## Instalace
 
 - **Python**: Hlavní programovací jazyk.
   - `Python` - version 3.12.10 nebo vyšší
@@ -30,6 +19,11 @@ Volby do Poslanecké sněmovny Parlamentu České republiky konané ve dnech 20.
   - `BeautifulSoup` (z balíčku `beautifulsoup4`) - Pro analýzu HTML.
   - `csv` - Pro práci s CSV soubory.
   - `argparse` - Rozhraní pro příkazovou řádku.
+  
+- Knihovný, které jsou použity v kódu jsou uložené requirements.txt.
+Pro instalaci doporučuji použit nové virtuální prostředí a s nainstalovaným manažerem spustit následovně:
+```pip3 -- version```
+```pip3 instal -r requirements.txt```
 
 ## Spouštění programu
 
@@ -37,18 +31,35 @@ Spuštění souboru main.py v rámci přikázového řádku požaduje 2 argument
    ```bash
    python3 main.py "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=14&xnumnuts=8105" "vysledky_opava.csv"
    ```
-Následně se Vám stáhnou výsledky jako soubor s příponou ``.csv``.
+
+## Struktura projektu
+- `main.py` - Hlavní skript pro stažení a zpracování dat.
+- `vysledky_opava.csv` - Výstupní soubor obsahující zpracovaná data.
+- `requirements.txt` - Seznam závislostí potřebných pro spuštění projektu.\
+
+## Ukázka kódu
+
+```Kód obce,Název obce,Voliči v seznamu,Vydané obálky,Platné hlasy```
+
+```512974,Bělá,559,379,22,0,0,27,1,9,15,3,8,5,1,1,18,0,1,15,150```
+
+```506192,Bohuslavice,1 380,908,,48,3,1,48,0,20,32,3,6,9,1,2```
+
+```506214,Bolatice,3 533,2 309,,99,1,3,126,1,41,101,18,11```
+
+```554197,Branka u Opavy,887,593,,37,0,0,60,0,12,59,0,1,4,1,1```
+
 
 ## Průběh stahování
 `Stahuji data z vybraneho URL: https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=14&xnumnuts=8105`
 
-`ukladam do souboru: vysledky_opava.csv`
+`Ukládám do souboru: vysledky_opava.csv`
 
-`ukoncuji main.py`
+`Ukončuji election-scraper`
 
 ## Zdroje
 **ENGETO**
 
-https://engeto.cz/
+[https://engeto.cz/](https://engeto.cz/)
 
 Poděkování všem lektorům za skvělý kurz!
